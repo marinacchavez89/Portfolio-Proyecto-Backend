0b17d5afa56d7651d575d7e1e43fb7d1fc569993
Usuario <marinachavez89@gmail.com>
2022-07-16 15:48:04 -0300

    modif cors

--- src/main/java/com/portfolio/MCC/Controller/LoginController.java
+++ src/main/java/com/portfolio/MCC/Controller/LoginController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080/auth/login","https://portfoliomcc-38e6c.firebaseapp.com/#/iniciar-sesion", "http://localhost:4200/", 
-    "https://portfoliomcc-38e6c.web.app/#/portfolio", "https://portfoliomcc-38e6c.web.app", "http://localhost:4200/#/iniciar-sesion"})
+@CrossOrigin(origins = {"https://portfoliomcc-38e6c.firebaseapp.com/", "http://localhost:4200/", "https://portfoliomcc-38e6c.web.app"})
 public class LoginController {
     @PostMapping("auth/login")
     public LoginResponse login(@RequestBody LoginRequest LoginRequest){
